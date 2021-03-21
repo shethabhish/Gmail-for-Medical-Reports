7c9181986ade28104fa797c5511871fb8cbf4125
shethabhish <shethabhish@gmail.com>
2021-03-20 18:45:10 -0700

    data and reports

---
+++ gmail-medical-reports/src/Data/Data.js
@@ -0,0 +1,157 @@
+import raw from "raw.macro"
+
+export const DATA = [
+    {
+        id: 0,
+        location: raw('./11-0.txt')    },
+    {
+        id: 1,
+        location: raw('./16-0.txt')    },
+    {
+        id: 2,
+        location: raw('./23-0.txt')    },
+    {
+        id: 3,
+        location: raw('./43-0.txt')    },
+    {
+        id: 4,
+        location: raw('./46-0.txt')    },
+    {
+        id: 5,
+        location: raw('./55.txt')    },
+    {
+        id: 6,
+        location: raw('./74-0.txt')    },
+    {
+        id: 7,
+        location: raw('./76-0.txt')    },
+    {
+        id: 8,
+        location: raw('./84-0.txt')    },
+    {
+        id: 9,
+        location: raw('./98-0.txt')    },
+    {
+        id: 10,
+        location: raw('./120-0.txt')    },
+    {
+        id: 11,
+        location: raw('./158-0.txt')    },
+    {
+        id: 12,
+        location: raw('./160-0.txt')    },
+    {
+        id: 13,
+        location: raw('./174-0.txt')    },
+    {
+        id: 14,
+        location: raw('./205-0.txt')    },
+    {
+        id: 15,
+        location: raw('./215-0.txt')    },
+    {
+        id: 16,
+        location: raw('./219-0.txt')    },
+    {
+        id: 17,
+        location: raw('./345-0.txt')    },
+    {
+        id: 18,
+        location: raw('./408-0.txt')    },
+    {
+        id: 19,
+        location: raw('./844-0.txt')    },
+    {
+        id: 20,
+        location: raw('./1080-0.txt')    },
+    {
+        id: 21,
+        location: raw('./1184-0.txt')    },
+    {
+        id: 22,
+        location: raw('./1232-0.txt')    },
+    {
+        id: 23,
+        location: raw('./1250-0.txt')    },
+    {
+        id: 24,
+        location: raw('./1260-0.txt')    },
+    {
+        id: 25,
+        location: raw('./1342-0.txt')    },
+    {
+        id: 26,
+        location: raw('./1400-0.txt')    },
+    {
+        id: 27,
+        location: raw('./1661-0.txt')    },
+    {
+        id: 28,
+        location: raw('./1952-0.txt')    },
+    {
+        id: 29,
+        location: raw('./2542-0.txt')    },
+    {
+        id: 30,
+        location: raw('./2554-0.txt')    },
+    {
+        id: 31,
+        location: raw('./2591-0.txt')    },
+    {
+        id: 32,
+        location: raw('./2600-0.txt')    },
+    {
+        id: 33,
+        location: raw('./2701-0.txt')    },
+    {
+        id: 34,
+        location: raw('./2814-0.txt')    },
+    {
+        id: 35,
+        location: raw('./2852-0.txt')    },
+    {
+        id: 36,
+        location: raw('./4300-0.txt')    },
+    {
+        id: 37,
+        location: raw('./25344-0.txt')    },
+    {
+        id: 38,
+        location: raw('./42108-0.txt')    },
+    {
+        id: 39,
+        location: raw('./64317-0.txt')    },
+    {
+        id: 40,
+        location: raw('./64851-0.txt')    },
+    {
+        id: 41,
+        location: raw('./64856-0.txt')    },
+    {
+        id: 42,
+        location: raw('./64857-0.txt')    },
+    {
+        id: 43,
+        location: raw('./64860-0.txt')    },
+    {
+        id: 44,
+        location: raw('./64861-0.txt')    },
+    {
+        id: 45,
+        location: raw('./64864-0.txt')    },
+    {
+        id: 46,
+        location: raw('./64865-0.txt')    },
+    {
+        id: 47,
+        location: raw('./pg514.txt')    },
+    {
+        id: 48,
+        location: raw('./pg5200.txt')    },
+    {
+        id: 49,
+        location: raw('./pg57775.txt')    },
+    {
+        id: 50,
+        location: raw('./730-0.txt')    }
+];
